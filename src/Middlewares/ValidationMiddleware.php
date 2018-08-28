aad61318e10bb38311137a268d801e1d94e77eb2
Lap Dam <xuanlap93@gmail.com>
2018-08-28 11:34:18 +0700

    fix validation feature

--- src/Middlewares/ValidationMiddleware.php
+++ src/Middlewares/ValidationMiddleware.php
@@ -8,9 +8,13 @@ use Megaads\Apify\Controllers\BaseController;
 class ValidationMiddleware extends BaseController {
 
     public function handle($request, Closure $next) {
-
-        if ($request->isMethod('POST')||$request->isMethod('put')||$request->isMethod('patch')) {
-            $entity = explode("/", explode("/api/", $request->url())[1])[0];
+        $prefix = env('APIFY_PREFIX_URL', 'api');
+        if ($request->isMethod('POST')||$request->isMethod('PUT')||$request->isMethod('PATCH')) {
+            if (!empty($prefix)) {
+                $entity = explode("/", explode($prefix, $request->url())[1])[0];
+            }else{
+                $entity = $request->segment(2);
+            }
             $model = $this->getModel($entity);
             if (isset($model->rules) || method_exists($model,"getRules")) {
                 $rules = isset($model->rules)?$model->rules:$model->getRules($request->input('id',-1));
