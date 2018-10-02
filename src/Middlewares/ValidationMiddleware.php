2814e1a99a8ccc237233e0e1b31885d4d741d5fc
Phu Luong <phult.contact@gmail.com>
2018-10-02 13:47:03 +0700

    Add APIFY_VALIDATION_ENABLE config

--- src/Middlewares/ValidationMiddleware.php
+++ src/Middlewares/ValidationMiddleware.php
@@ -5,24 +5,29 @@ namespace Megaads\Apify\Middlewares;
 use Closure;
 use Megaads\Apify\Controllers\BaseController;
 
-class ValidationMiddleware extends BaseController {
+class ValidationMiddleware extends BaseController
+{
 
-    public function handle($request, Closure $next) {
-        $prefix = env('APIFY_PREFIX_URL', 'api');
-        if ($request->isMethod('POST')||$request->isMethod('PUT')||$request->isMethod('PATCH')) {
-            if (!empty($prefix)) {
-                $entity = explode("/", explode('/'.$prefix.'/', $request->url())[1])[0];
-            }else{
-                $entity = $request->segment(2);
-            }
-            $model = $this->getModel($entity);
-            if (isset($model->rules) || method_exists($model,"getRules")) {
-                $rules = isset($model->rules)?$model->rules:$model->getRules($request->input('id',-1));
-                $messages = [];
-                if (isset($model->messages)) {
-                    $messages = $model->messages;
+    public function handle($request, Closure $next)
+    {
+        $validationEnable = env('APIFY_VALIDATION_ENABLE', false);
+        if ($validationEnable) {
+            $prefix = env('APIFY_PREFIX_URL', '');
+            if ($request->isMethod('POST') || $request->isMethod('PUT') || $request->isMethod('PATCH')) {
+                if (!empty($prefix)) {
+                    $entity = explode("/", explode('/' . $prefix . '/', $request->url())[1])[0];
+                } else {
+                    $entity = $request->segment(2);
                 }
-                $this->validate($request, $rules,$messages);
+                $model = $this->getModel($entity);
+                if (isset($model->rules) || method_exists($model, "getRules")) {
+                    $rules = isset($model->rules) ? $model->rules : $model->getRules($request->input('id', -1));
+                    $messages = [];
+                    if (isset($model->messages)) {
+                        $messages = $model->messages;
+                    }
+                    $this->validate($request, $rules, $messages);
+                }
             }
         }
         $response = $next($request);
